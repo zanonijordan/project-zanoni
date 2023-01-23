1bb70e9050c43f0d974c13821a2ee4205ce04af5
JORDAN HENRIQUE MOREIRA ZANONI <52969396+zanonijordan@users.noreply.github.com>
2023-01-23 16:49:44 +0100

    reveal speed

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,11 +3,11 @@ window.sr = ScrollReveal({ reset: true });
 
 sr.reveal('.intro-img', {duration: 2000});
 sr.reveal('.text-intro', {duration: 2500});
-sr.reveal('.p-intro', {duration: 4000});
+sr.reveal('.p-intro', {duration: 3000});
 
 sr.reveal('#ale-lado', {duration: 2000});
 sr.reveal('.aboutMe-description-text', {duration: 2500});
-sr.reveal('.list-aboutMe', {duration: 4000});
+sr.reveal('.list-aboutMe', {duration: 3000});
 
 /* --initial position */
 let mainLocation = window.pageYOffset;
